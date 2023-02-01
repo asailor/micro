87ea057925ccb69dd7aea6c108b3de19c6882eee
xs <1772689945@qq.com>
2023-02-01 09:57:07 +0800

    mirco

---
+++ system/src/test/java/com/gdsig/system/service/RocketMqTestServiceTest.java
@@ -0,0 +1,15 @@
+package com.gdsig.system.service;
+
+import org.junit.Test;
+
+/**
+ * @author xs
+ * @date 2022/12/22上午 10:12
+ */
+public class RocketMqTestServiceTest extends RocketMqTestServiceImpl {
+
+    @Test
+    public void testSend() {
+        send();
+    }
+}
